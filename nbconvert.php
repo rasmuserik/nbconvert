cf4d8e138fe2aa60219fee80f3d5955392968e2f
RasmusErik Voel Jensen <github-rasmuserik@solsort.dk>
2018-05-17 07:23:44 +0200

    no border etc.

--- nbconvert.php
+++ nbconvert.php
@@ -15,68 +15,6 @@ function nbconvert_handler($atts) {
   return $nb_output;
 }
 
-function nbconvert_get_most_recent_git_change_for_file_from_api($url) {
-
-  $url_list = explode('/', $url);
-
-  $owner = $url_list[3];
-  $repo = $url_list[4];
-  $branch = $url_list[6];
-  $path = implode("/", array_slice($url_list, 7));
-
-  $request_url = 'https://api.github.com/repos/'.$owner.'/'.$repo.'/commits/'.$branch.'?path='. $path.'&page=1';
-
-  $context_params = array(
-    'http' => array(
-      'method' => 'GET',
-      'user_agent' => 'Bogus user agent',
-      'timeout' => 1
-    )
-  );
-
-  
-  $res = file_get_contents($request_url, FALSE, stream_context_create($context_params));
-
-  $datetime = json_decode($res, true)['commit']['committer']['date'];
-
-  $max_datetime = strtotime($datetime);
-  $max_datetime_f = date('d/m/Y H:i:s', $max_datetime);
-
-  return $max_datetime_f;
-}
-
-/*
-function get_most_recent_git_change_for_file($url) {
-  
-  $url_list = explode('/', $url);
-  $url_list[5] = 'blame';
-  $new_url = implode("/", $url_list);
-  //Load the HTML page
-  $html = file_get_contents($new_url);
-  
-  //Create a new DOM document
-  $dom = new DOMDocument;
-  libxml_use_internal_errors(true);
-  $dom->loadHTML($html);
-  
-  // Get all time-ago tags
-  $time_agos = $dom->getElementsByTagName('time-ago');
-
-  $mostRecent= 0;
-  foreach($time_agos as $time_ago){
-    $datetime = $time_ago->getAttribute('datetime');
-    $curDate = strtotime($datetime);
-    if ($curDate > $mostRecent) {
-       $mostRecent = $curDate;
-    }
-  }
-
-  $max_date = date('d/m/Y H:i:s', $mostRecent);
-  return $max_date;
-  
-}
-*/
-
 function nbconvert_function($atts) {
   //process plugin
   extract(shortcode_atts(array(
@@ -87,18 +25,7 @@ function nbconvert_function($atts) {
   $html = file_get_contents("https://nbviewer.jupyter.org/url/" . $clean_url);
   $nb_output = nbconvert_getHTMLByID('notebook-container', $html);
 
-  $last_update_date_time = nbconvert_get_most_recent_git_change_for_file_from_api($url);
-
-  $converted_nb = '<div class="notebook">
-    <div class="nbconvert-labels">
-      <label class="github-link">
-        <a href="'.$url.'" target="_blank">Check it out on github</a>
-        <label class="github-last-update"> Last updated: '.$last_update_date_time.'</label>
-      </label>
-      </div>
-    <div class="nbconvert">'.$nb_output.'
-    </div>
-  </div>';
+  $converted_nb = '<div class="notebook"><div class="nbconvert">'.$nb_output.'</div></div>';
 
   //send back text to calling function
   return $converted_nb;
